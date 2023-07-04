4d1c2a74a6ad5e28b79f5f4944951fba8c320962
Omutnykh Oleg <omutnih1234@gmail.com>
2023-07-05 01:47:23 +0500

    Добавлена функция десериализации запроса

--- Program.cs
+++ Program.cs
@@ -1,20 +1,44 @@
 using System.Text;
 using AutoConfirmEventsInTimepad.Models;
 using TimepadClient;
+using System.Text.Json;
+
+WebHookTicket DeserializeWebHookTicketBody<T>(Stream stream)
+{
+    try
+    {
+        return JsonSerializer.Deserialize<WebHookTicket>(stream);
+    }
+    catch (JsonException e)
+    {
+        throw new Exception(e.Message);
+    }
+}
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<string> emailDomens = new List<string>() {"@timepath.ru", "@pminst.ru"};
+List<string> emailDomens = new List<string>
+{
+    "@timepath.ru", 
+    "@pminst.ru"
+};
 
 
 app.Run(async (context) =>
 {
-    Ticket ticket = new Ticket("5184211:83845994", 215813, 29963, "4955686", "2015-07-24 19:04:37", 361138, "забронировано", "booked", "test-mail@ya.ru", "Смирнов", "Владимир", false, "83845994", "83845994", 1000, null);
-    await context.Response.WriteAsJsonAsync(ticket);
+    var response = context.Response;
+    var request = context.Request;
+    var requestBody = request.Body;
+    var stringRequestBody = DeserializeWebHookTicketBody<WebHookTicket>(requestBody);
+    //var requestBody = request.ReadFromJsonAsync<WebHookTicket>();
+    var currentEmail = stringRequestBody.email;
+    //var payload = Newtonsoft.Json.JsonConvert.DeserializeObject<WebHookTicket>(request);
+    WebHookTicket ticket = new WebHookTicket("5184211:83845994", 215813, 29963, "4955686", "2015-07-24 19:04:37", 361138, "забронировано", "booked", "test-mail@ya.ru", "Смирнов", "Владимир", false, "83845994", "83845994", 1000, null);
+    await response.WriteAsJsonAsync(ticket);
     StringBuilder email = new StringBuilder(ticket.email);
     email.Remove(0, ticket.email!.IndexOf('@'));
-    await context.Response.WriteAsync(email.ToString());
+    await response.WriteAsync(email.ToString());
     if (emailDomens.Contains(email.ToString()))
     {
         TimepadClient.TimepadClient timepadClient = new TimepadClient.TimepadClient(new HttpClient());
