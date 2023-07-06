ddb3c84fd6eb775227196456a1d5f20d6f8529b9
Omutnykh Oleg <omutnih1234@gmail.com>
2023-07-06 18:22:09 +0500

    Initial commit

--- Program.cs
+++ Program.cs
@@ -32,27 +32,37 @@ app.Run(async (context) =>
 {
     var response = context.Response;
     var request = context.Request;
-    var requestBody = request.Body;
-    app.Logger.LogInformation($"Info: {requestBody} Time: {DateTime.Now.ToLongTimeString()}");
-    // try
-    // {
-    //     var stringRequestBody = JsonSerializer.Deserialize(requestBody, WebHookTicket);
-    // }
-    var stringRequestBody = await DeserializeWebHookTicketBody<WebHookTicket>(requestBody);
-    var currentEmail = stringRequestBody.email;
-    //var payload = Newtonsoft.Json.JsonConvert.DeserializeObject<WebHookTicket>(request);
-    //WebHookTicket ticket = new WebHookTicket("5184211:83845994", 215813, 29963, "4955686", "2015-07-24 19:04:37", 361138, "забронировано", "booked", "test-mail@ya.ru", "Смирнов", "Владимир", false, "83845994", "83845994", 1000, null);
-    //await response.WriteAsJsonAsync(ticket);
-    StringBuilder email = new StringBuilder(currentEmail);
-    email.Remove(0, currentEmail!.IndexOf('@'));
-    await response.WriteAsync(email.ToString());
-    
-    if (emailDomens.Contains(email.ToString()))
+    try
     {
-        TimepadClient.TimepadClient timepadClient = new TimepadClient.TimepadClient(new HttpClient());
-        await timepadClient.ApproveEventOrderAsync(stringRequestBody.event_id, int.Parse(stringRequestBody.order_id!));
+        var webHookTicket = await request.ReadFromJsonAsync<WebHookTicket>();
+        //var requestBody = request.Body;
+        app.Logger.LogInformation($"Info: {webHookTicket!.ToString()} Time: {DateTime.Now.ToLongTimeString()}");
+        if (webHookTicket != null)
+        {
+            var currentEmail = webHookTicket.email;
+            //var payload = Newtonsoft.Json.JsonConvert.DeserializeObject<WebHookTicket>(request);
+            //WebHookTicket ticket = new WebHookTicket("5184211:83845994", 215813, 29963, "4955686", "2015-07-24 19:04:37", 361138, "забронировано", "booked", "test-mail@ya.ru", "Смирнов", "Владимир", false, "83845994", "83845994", 1000, null);
+            //await response.WriteAsJsonAsync(ticket);
+            StringBuilder email = new StringBuilder(currentEmail);
+            email.Remove(0, currentEmail!.IndexOf('@'));
+            await response.WriteAsync(email.ToString());
+            
+            if (emailDomens.Contains(email.ToString()))
+            {
+                TimepadClient.TimepadClient timepadClient = new TimepadClient.TimepadClient(new HttpClient());
+                await timepadClient.ApproveEventOrderAsync(webHookTicket.event_id, int.Parse(webHookTicket.order_id!));
+            }
+        }
+        //app.Logger.LogInformation($"Info: {requestBody.ToString()} Time: {DateTime.Now.ToLongTimeString()}");
+        // try
+        // {
+        //     var stringRequestBody = JsonSerializer.Deserialize(requestBody, WebHookTicket);
+        // }
+        //var stringRequestBody = await DeserializeWebHookTicketBody<WebHookTicket>(requestBody);
+        
     }
-
+    catch
+    {}
 });
 
 app.Run();
